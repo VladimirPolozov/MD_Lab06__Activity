f1824621858da3951c116586780e4d14b4cd2fca
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-11-28 13:40:14 +0700

    complete task (without test)

---
+++ app/src/main/java/com/example/md_lab06__activity/Photo.kt
@@ -0,0 +1,13 @@
+package com.example.md_lab06__activity
+
+data class Photo(
+    val id: String,
+    val owner: String,
+    val secret: String,
+    val server: String,
+    val farm: Int,
+    val title: String,
+    val isPublic: Int,
+    val isFriend: Int,
+    val isFamily: Int
+)
